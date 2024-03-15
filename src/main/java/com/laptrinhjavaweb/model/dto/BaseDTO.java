ca47bc7c4fef46736e6321813b6c7d0453b735ca
Nguyễn Quang Vinh <139441823+Nqvinh7603@users.noreply.github.com>
2024-03-15 09:21:53 +0700

    update project

---
+++ src/main/java/com/laptrinhjavaweb/model/dto/BaseDTO.java
@@ -0,0 +1,13 @@
+package com.laptrinhjavaweb.model.dto;
+
+public class BaseDTO {
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
