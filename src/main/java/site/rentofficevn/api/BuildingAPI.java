4c757aef42287a661fb80b902b5f3d85821e2759
Nguyễn Quang Vinh <139441823+Nqvinh7603@users.noreply.github.com>
2024-03-27 22:45:21 +0700

    load file properties

--- src/main/java/site/rentofficevn/api/BuildingAPI.java
+++ src/main/java/site/rentofficevn/api/BuildingAPI.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +20,13 @@ import site.rentofficevn.service.BuildingService;
 
 @RestController
 @RequestMapping("/api/building")
+@PropertySource("classpath:application.properties")
 public class BuildingAPI {
 	@Autowired
 	private BuildingService buildingService;
 
+	@Value("${anh.yeu.em}")
+	private String anhYeuEm;
 	// Lấy chi tiết tòa nhà
 	@GetMapping("/{buildingid}")
 	public BuildingDTO getBuildingDetail(@PathVariable(value = "buildingid") Long buildingId) {
@@ -33,7 +38,7 @@ public class BuildingAPI {
 	@GetMapping
 	public List<BuildingSearchResponse> findBuilding(@RequestParam(required = false) Map<String, String> buildingSearch,
 			@RequestParam(required = false) List<String> buildingTypes) throws SQLException {
-
+		System.out.print(anhYeuEm);
 		return buildingService.getBuildingList(buildingSearch, buildingTypes);
 	}
 }
