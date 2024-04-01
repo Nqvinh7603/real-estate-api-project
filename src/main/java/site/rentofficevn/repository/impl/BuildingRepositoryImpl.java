94f40da24297391a30fde64a063969d806ec7bfd
Nguyễn Quang Vinh <139441823+Nqvinh7603@users.noreply.github.com>
2024-04-01 08:48:24 +0700

    update 1/4

--- src/main/java/site/rentofficevn/repository/impl/BuildingRepositoryImpl.java
+++ src/main/java/site/rentofficevn/repository/impl/BuildingRepositoryImpl.java
@@ -15,12 +15,9 @@ public class BuildingRepositoryImpl	 extends JdbcRepositoryImpl<BuildingEntity> 
 	@Override
 	public List<BuildingEntity> findBuilding(Map<String, Object> buildingSearch, List<String> buildingTypes) {
 		String name = MapUtils.getObject(buildingSearch, "name", String.class);
-		System.out.println(name);
 		Long staffId = MapUtils.getObject(buildingSearch, "staffid", Long.class);
-		System.out.println(staffId);
 		Integer numberOfBasement = MapUtils.getObject(buildingSearch, "numberofbasement", Integer.class);
-		System.out.println(numberOfBasement);
-		//String query = "select * from building " + SystemConstant.WHERE_ONE_EQUAL_ONE;
+		
 		return null;
 	}
 }
