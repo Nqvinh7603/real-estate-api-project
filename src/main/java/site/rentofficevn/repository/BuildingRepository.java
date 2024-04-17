1b1eb2ddbf5124f1428f20e954f30fe2deef3eae
Nguyễn Quang Vinh <139441823+Nqvinh7603@users.noreply.github.com>
2024-04-17 11:06:47 +0700

    merge branch Jpa

--- src/main/java/site/rentofficevn/repository/BuildingRepository.java
+++ src/main/java/site/rentofficevn/repository/BuildingRepository.java
@@ -1,7 +1,6 @@
 package site.rentofficevn.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import site.rentofficevn.repository.custom.BuildingRepositoryCustom;
 import site.rentofficevn.repository.entity.BuildingEntity;
 public interface BuildingRepository extends BuildingRepositoryCustom,JpaRepository<BuildingEntity, Long> {
